f9e9b7cf5eef707b60e71f1dcf891064527373c4
Suyank Saraswat <31132741+suyanksaraswat@users.noreply.github.com>
2020-08-03 03:31:25 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
 
+//Put your config from firebase here...
 const firebaseConfig = {
-	apiKey: "AIzaSyAj8Qi4xRY6jb4b0IFUcU1LvNblaVqG5GM",
-	authDomain: "tinder-clone-2ecc4.firebaseapp.com",
-	databaseURL: "https://tinder-clone-2ecc4.firebaseio.com",
-	projectId: "tinder-clone-2ecc4",
-	storageBucket: "tinder-clone-2ecc4.appspot.com",
-	messagingSenderId: "123457856691",
-	appId: "1:123457856691:web:e0b33c3aff924dd705bcc6",
-	measurementId: "G-7Z3LXK7QY3"
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
+	measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
